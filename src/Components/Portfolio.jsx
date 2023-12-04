39ffa15e8e89664b8527704178283bb1c240061a
Alex Er <113337139+OscarEncarnacion@users.noreply.github.com>
2023-12-04 08:22:26 +0000

    Commit made from VS Code Azure Static Web Apps

--- src/Components/Portfolio.jsx
+++ src/Components/Portfolio.jsx
@@ -19,7 +19,7 @@ import React from "react";
  * Need an image? Check out https://unsplash.com to download a photo you
  * freely use on your site.
  */
-import image from "../images/design-desk.jpeg";
+import image from "../images/mountains.jpg";
 
 const imageAltText = "desktop with books and laptop";
 
@@ -31,28 +31,26 @@ const imageAltText = "desktop with books and laptop";
  */
 const projectList = [
   {
-    title: "10 Things To Know About Azure Static Web Apps 🎉",
+    title: "Curve Fitting",
     description:
-      "Collaboration to create a beginner friendly article to help explain Azure Static Web Apps and tooling to get started.",
-    url: "https://dev.to/azure/10-things-to-know-about-azure-static-web-apps-3n4i",
+      "Desktop application that predicts sales based on the advertising investment entered. Calculations based on data from the Benetton case.",
+    url: "https://github.com/OscarEncarnacion/CurveFitting",
   },
   {
-    title: "Web Development for Beginners",
-    description:
-      "Contributed sketch note imagery to accompany each lesson. These help provide visual representation of what is being taught.",
-    url: "https://github.com/microsoft/web-dev-for-beginners",
+    title: "Dijkstra",
+    description: "Algoritmo de Dijkstra.",
+    url: "https://github.com/OscarEncarnacion/Dijkstra",
   },
   {
-    title: "My Resume Site",
+    title: "Conductas de consumo en CLIPS",
     description:
-      "Created from Microsoft's resume workshop and deployed to GitHub pages. Includes my experience and design abilities.",
-    url: "https://github.com/microsoft/workshop-library/tree/main/full/build-resume-website",
+      "Se presenta una base de conocimiento sencilla que ofrece promociones en base a los productos comprados y a las tarjetas de crédito utilizadas para pagar.",
+    url: "https://github.com/OscarEncarnacion/ConductasConsumoClips",
   },
   {
-    title: "GitHub Codespaces and github.dev",
-    description:
-      "Video interview to explain when to use GitHub.dev versus GitHub Codespaces, and how best to use each tool.",
-    url: "https://www.youtube.com/watch?v=c3hHhRME_XI",
+    title: "Salon de eventosMalvaterra",
+    description: "WebSite in PHP for gestioning an event salon.",
+    url: "https://github.com/celia-bl/Proyecto_Malvaterra",
   },
 ];
 
